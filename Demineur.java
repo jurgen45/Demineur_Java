b154066a897aafbae32d5dcee7e2c941956b3ed7
nathan <nathan.bertholier@gmail.com>
2018-04-05 21:59:48 +0200

    ui

--- Demineur.java
+++ Demineur.java
@@ -323,18 +323,13 @@ public class Demineur extends JFrame implements ActionListener,MouseListener {
     for (int i = 0; i < colonne; i++) {
             for (int f = 0; f < ligne; f++) {
                 if (e.getSource()==tabCase[f][i]&& tabCase[f][i].etatMine()==false&&tabCase[f][i].getValide()==false && e.getModifiers() == MouseEvent.BUTTON3_MASK) {
-                   
-                                    
-                                    tabCase[f][i].setIcon(new ImageIcon("flag1.png"));
-                                
-                    
+                                     tabCase[f][i].setEtat();
                 }
                 if(e.getSource()==tabCase[f][i]&& tabCase[f][i].etatMine()==true && e.getModifiers() == MouseEvent.BUTTON3_MASK)
                     {
-                    tabCase[f][i].setIcon(new ImageIcon("flag1.png"));
-                    tabCase[f][i].setEtat();
-                }
-               
+                        tabCase[f][i].setEtat();
+                    }
+                
             }
         }
         for (int i = 0; i < colonne; i++) {
