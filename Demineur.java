00bc977eb2681c9db24b67649a1ccddce678da37
nathan <nathan.bertholier@gmail.com>
2018-04-05 11:28:01 +0200

    maj Case

--- Demineur.java
+++ Demineur.java
@@ -7,7 +7,7 @@ public class Demineur extends JFrame implements ActionListener {
     private int nbligne;
     private int nbColonne;
     private int nbMine;
-    private JButton[][] tabJButton=null;
+    private Case[][] tabCase=null;
    public Demineur(int ligne,int colonne,int mine)
    {
         super();
@@ -17,11 +17,11 @@ public class Demineur extends JFrame implements ActionListener {
         this.setVisible(true);
         GridLayout grid=new GridLayout(colonne,ligne);
         this.setLayout(grid);
-        tabJButton=new JButton[ligne][colonne];
+        tabCase=new Case[ligne][colonne];
             System.out.println(ligne+"/"+colonne + "/"+mine);
             for (int i = 0; i < colonne; i++) {
             for (int f = 0; f < ligne; f++) {
-                tabJButton[f][i]=new JButton();
+                tabCase[f][i]=new Case(false);
             }
         }
             double aleadouble=Math.random() * 10;
@@ -33,8 +33,7 @@ public class Demineur extends JFrame implements ActionListener {
                     aleadouble = Math.random() * 100;
                     alea = (int) aleadouble;
                     if (alea==1&&compteurAleaMine<mine) {
-                        tabJButton[f][i] = new JButton("*");
-                        tabJButton[f][i].setBackground(Color.BLACK);
+                        tabCase[f][i] = new Case(true);
                         System.out.println("mine :"+f + "/" + i);
                         compteurAleaMine++;
                     }
@@ -45,9 +44,18 @@ public class Demineur extends JFrame implements ActionListener {
             }
             for (int i = 0; i < colonne; i++) {
                 for (int f = 0; f < ligne; f++) {
-            this.add(tabJButton[f][i]);
+            this.add(tabCase[f][i]);
                 }
             }
+            for (int i = 0; i < colonne; i++) {
+                     for (int f = 0; f < ligne; f++) {
+                        if (tabCase[f][i].etatMine()==true) {
+                            System.out.println("mine black");
+                            tabCase[f][i].setBackground(Color.BLACK);
+                        }
+                     }
+             }
+
             
             
         }
