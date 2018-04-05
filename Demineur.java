c79c292c7fccb0b3a15ecccf8a7c21c2f1d689f3
nathan <nathan.bertholier@gmail.com>
2018-04-05 21:57:47 +0200

    f

--- Demineur.java
+++ Demineur.java
@@ -323,7 +323,6 @@ public class Demineur extends JFrame implements ActionListener,MouseListener {
     for (int i = 0; i < colonne; i++) {
             for (int f = 0; f < ligne; f++) {
                 if (e.getSource()==tabCase[f][i]&& tabCase[f][i].etatMine()==false&&tabCase[f][i].getValide()==false && e.getModifiers() == MouseEvent.BUTTON3_MASK) {
-<<<<<<< HEAD
                    
                                     
                                     tabCase[f][i].setIcon(new ImageIcon("flag1.png"));
@@ -332,18 +331,10 @@ public class Demineur extends JFrame implements ActionListener,MouseListener {
                 }
                 if(e.getSource()==tabCase[f][i]&& tabCase[f][i].etatMine()==true && e.getModifiers() == MouseEvent.BUTTON3_MASK)
                     {
-                        
-                        tabCase[f][i].setIcon(new ImageIcon("flag1.png"));
-                       
-=======
-                                     tabCase[f][i].setEtat();
+                    tabCase[f][i].setIcon(new ImageIcon("flag1.png"));
+                    tabCase[f][i].setEtat();
                 }
-                if(e.getSource()==tabCase[f][i]&& tabCase[f][i].etatMine()==true && e.getModifiers() == MouseEvent.BUTTON3_MASK)
-                    {
-                        tabCase[f][i].setEtat();
->>>>>>> d4e82b1238756c4453513a6747b5f5d3356db58e
-                    }
-                
+               
             }
         }
         for (int i = 0; i < colonne; i++) {
